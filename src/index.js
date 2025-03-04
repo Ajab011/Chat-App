d370a6dfe3f82321c0f01ddc8c6c91b95bd862a0
Ajab011 <ajabflexwala@gmail.com>
2025-03-04 22:49:02 +0530

    adding cloudinary and other features

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-Parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js"
-
+import messageRoutes from "./routes/message.route.js"
 dotenv.config();
 const app=express();
 // app.get("/",(req,res)=>{
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 app.listen(5001,()=>{
     console.log("server is running 5001");
--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
-import  User from "../models/user.model.js"
+import User from "../models/user.model.js";
 export const protectRoute=async(req,res,next)=>{
     try{
         const token=req.cookies.jwt
         if(!token){
-            return res.statys(401).json({message:"Unauthorized-No Token Provoded"});
+            return res.status(401).json({message:"Unauthorized-No Token Provided"});
         
         }
         const decoded=jwt.verify(token,process.env.JWT_SECRET)
@@ -16,7 +16,7 @@ export const protectRoute=async(req,res,next)=>{
         if(!user){
             return res.status (404).json({message:"User not found"})
         }
-        req.user=user;
+        req.user = user;
         next()
     }
     catch(error){
